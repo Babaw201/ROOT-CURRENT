da69466ae4019880fb83319c71bb68241406c652
Babaw201 <baptiste.werner@gmail.com>
2026-07-31 15:59:51 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: '/PlantMind/',
+  base: '/ROOT-CURRENT/',
   plugins: [react(), tailwindcss()],
 })
