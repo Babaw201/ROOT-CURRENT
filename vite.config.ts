8d850b8d431bd2a000cd0391d649ae64d3710d15
Babaw201 <baptiste.werner@gmail.com>
2026-08-01 11:06:49 +0200

    Store PlantMind modular import archive

--- vite.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
-
-export default defineConfig({
-  base: '/ROOT-CURRENT/',
-  plugins: [react(), tailwindcss()],
-})
